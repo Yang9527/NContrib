cc08c2072e37d4d2e5ee4a596e386ade62239920
yangmingmin <yangmingmin@zuoyebang.com>
2019-02-28 11:59:56 +0800

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Sun Jan 27 21:03:26 2019
 
 import numpy as np
 import cvxopt as opt
+import sys
 
 def load_data(filename):
     with open(filename) as fin:
@@ -14,6 +15,9 @@ def load_data(filename):
         lines = [line.strip().split() for line in lines if line.strip() != ""]
         lines = [[float(x) for x in line] for line in lines]
         return lines
+def format_rlt(x, length=5):
+    return ",".join(["%f"%x[i, 0] for i in range(length)])
+    
 
 def solve(records):
     """
@@ -30,10 +34,19 @@ def solve(records):
     G = opt.matrix(np.diag([-1,-1.0,-1,-1,-1]))
     h = opt.matrix(np.zeros(5))
     sol = opt.solvers.qp(P,q,G,h,A,b)
-    print(sol['x'])
+    x = sol['x']
+    return x
 
 
 if __name__ == "__main__":
-    data = load_data("data.txt")
-    solve(data[:2])
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    data = load_data(input_file)
+    N = int(len(data) / 2)
+    print(N)
+    with open(output_file, 'w') as fout:
+        for i in range(N):
+            x = solve(data[i:i+2])
+            fout.write(format_rlt(x))
+            fout.write("\r\n")
     
